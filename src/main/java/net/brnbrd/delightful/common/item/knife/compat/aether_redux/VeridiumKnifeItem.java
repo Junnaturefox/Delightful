e8299451f611ba3c9d751898a69ecff144972f99
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2024-07-18 21:29:12 -0400

    fix: Redux compat fixes

--- src/main/java/net/brnbrd/delightful/common/item/knife/compat/aether_redux/VeridiumKnifeItem.java
+++ src/main/java/net/brnbrd/delightful/common/item/knife/compat/aether_redux/VeridiumKnifeItem.java
@@ -1,91 +1,90 @@
 package net.brnbrd.delightful.common.item.knife.compat.aether_redux;
 
 import net.brnbrd.delightful.common.item.DelightfulItems;
-import net.brnbrd.delightful.common.item.knife.Knives;
 import net.brnbrd.delightful.common.item.knife.compat.aether.AetherKnifeItem;
 import net.minecraft.ChatFormatting;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.SwordItem;
 import net.minecraft.world.item.Tier;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
-import net.zepalesque.redux.item.ReduxItems;
-import net.zepalesque.redux.item.util.VeridiumItem;
-import org.jetbrains.annotations.NotNull;
+import net.zepalesque.redux.item.tools.VeridiumItem;
+import net.zepalesque.redux.item.util.TooltipUtils;
 import org.jetbrains.annotations.Nullable;
+
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 public class VeridiumKnifeItem extends AetherKnifeItem implements VeridiumItem {
-	public VeridiumKnifeItem(Tier tier, Properties properties) {
-		super("aether_redux", DelightfulItems.ingot("veridium"), tier, properties);
-	}
+    private final Supplier<? extends Item> uninfused;
 
-	public boolean isCharged(Item item) {
-		return item == Knives.INFUSED_VERIDIUM.get();
-	}
+    public VeridiumKnifeItem(Tier tier, Properties properties, Supplier<? extends Item> uninfused) {
+        super("aether_redux", DelightfulItems.ingot("veridium"), tier, properties);
+        this.uninfused = uninfused;
+    }
 
-	@Override
-	public @NotNull ItemStack getDefaultInstance() {
-		ItemStack stack = super.getDefaultInstance().copy();
-		return isCharged(this) ?
-			VeridiumItem.infuse(stack, VeridiumItem.MAXIUMUM_VERIDIUM_INFUSION)
-			: stack;
-	}
+    @Override
+    public Item getUninfusedItem(ItemStack stack) {
+        return this.uninfused.get();
+    }
 
-	@Override
-	public Item getReplacementItem(ItemStack itemStack) {
-		return this.isCharged(itemStack.getItem()) ? Knives.VERIDIUM.get() : Knives.INFUSED_VERIDIUM.get();
-	}
+    @Override
+    public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> tooltips, TooltipFlag advanced) {
+        MutableComponent infusion = Component.translatable("tooltip.aether_redux.infusion_charge", stack.getTag() == null ? 0 : stack.getTag().getByte(VeridiumItem.NBT_KEY)).withStyle(ChatFormatting.GRAY);
 
-	public void appendHoverText(@NotNull ItemStack pStack, @Nullable Level pLevel, @NotNull List<Component> comps, @NotNull TooltipFlag pIsAdvanced) {
-		if (this.enabled()) {
-			MutableComponent component = Component.translatable("tooltip.aether_redux.ambrosium_charge",
-				VeridiumItem.getInfusion(pStack)).withStyle(ChatFormatting.GRAY);
-			comps.add(component);
-			Component c = ReduxItems.TooltipUtils.SHIFT_OR_DEFAULT.apply(Component.translatable("gui.aether_redux.infusion_tooltip"));
-			comps.add(c);
-		}
-		super.appendHoverText(pStack, pLevel, comps, pIsAdvanced);
-	}
+        tooltips.add(infusion);
+        Component info = TooltipUtils.TOOLTIP_SHIFT_FOR_INFO.apply(Component.translatable("gui.aether_redux.infusion_info"));
+        tooltips.add(info);
+        super.appendHoverText(stack, level, tooltips, advanced);
+    }
 
-	public boolean hurtEnemy(@NotNull ItemStack pStack, @NotNull LivingEntity pTarget, @NotNull LivingEntity pAttacker) {
-		if (this.isCharged(pStack.getItem()) && !pAttacker.level().isClientSide() && (!(pAttacker instanceof Player) || !((Player)pAttacker).getAbilities().instabuild)) {
-			ItemStack stackReplacement = VeridiumItem.depleteInfusion(pStack, pAttacker);
-			if (!stackReplacement.getItem().equals(pStack.getItem())) {
-				pAttacker.setItemSlot(EquipmentSlot.MAINHAND, stackReplacement);
-			} else {
-				pStack.setTag(stackReplacement.getTag());
-			}
-		}
-		return super.hurtEnemy(pStack, pTarget, pAttacker);
-	}
+    @Override
+    public boolean mineBlock(ItemStack stack, Level level, BlockState state, BlockPos pos, LivingEntity user) {
+        // Call the vanilla method do do things like tool damaging
+        boolean bool = super.mineBlock(stack, level, state, pos, user);
+        if (!user.level().isClientSide()) {
+            boolean instaBreak = state.getDestroySpeed(level, pos) <= 0.0F;
+            // Avoid decreasing infusion on insta-break blocks
+            if (!instaBreak) {
+                int amount = stack.isCorrectToolForDrops(state) ? 1 : 2;
+                ItemStack transform = this.deplete(stack, user, amount);
+                if (!user.level().isClientSide() && transform != null && transform != stack) {
+                    user.setItemSlot(EquipmentSlot.MAINHAND, transform);
+                    if (user instanceof ServerPlayer sp) {
+                        this.sendSound(sp);
+                    }
+                }
+            }
+        }
+        return bool;
+    }
 
-	public boolean mineBlock(@NotNull ItemStack pStack, @NotNull Level pLevel, BlockState pState, @NotNull BlockPos pPos, @NotNull LivingEntity pEntityLiving) {
-		if (pState.getDestroySpeed(pLevel, pPos) != 0.0F && this.isCharged(pStack.getItem()) && !pEntityLiving.level().isClientSide()) {
-			if (pEntityLiving instanceof Player p) {
-				if (p.getAbilities().instabuild) {
-					return super.mineBlock(pStack, pLevel, pState, pPos, pEntityLiving);
-				}
-			}
-			ItemStack stackReplacement = VeridiumItem.depleteInfusion(pStack, pEntityLiving);
-			if (!stackReplacement.getItem().equals(pStack.getItem())) {
-				pEntityLiving.setItemSlot(EquipmentSlot.MAINHAND, stackReplacement);
-			} else {
-				pStack.setTag(stackReplacement.getTag());
-			}
-		}
-		return super.mineBlock(pStack, pLevel, pState, pPos, pEntityLiving);
-	}
+    @Override
+    public <T extends LivingEntity> int damageItem(ItemStack stack, int amount, T entity, Consumer<T> onBroken) {
+        return super.damageItem(stack, amount, entity, onBroken) * VeridiumItem.DURABILITY_DMG_MULTIPLIER;
+    }
 
-	public <T extends LivingEntity> int damageItem(ItemStack stack, int amount, T entity, Consumer<T> onBroken) {
-		return this.isCharged(stack.getItem()) ? amount * this.CHARGED_DAMAGE_MULTIPLIER : amount;
-	}
+
+    public static class Uninfused extends AetherKnifeItem {
+
+        public Uninfused(Tier tier, Properties properties) {
+            super("aether_redux", DelightfulItems.ingot("veridium"), tier, properties);
+        }
+
+        @Override
+        public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> tooltips, TooltipFlag advanced) {
+            Component info = TooltipUtils.TOOLTIP_SHIFT_FOR_INFO.apply(Component.translatable("gui.aether_redux.infusion_info"));
+            tooltips.add(info);
+            super.appendHoverText(stack, level, tooltips, advanced);
+        }
+    }
 }
